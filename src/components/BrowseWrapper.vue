810c52dafedf6447982bbf54000ddc309fda790c
Tray Northern <43824772+TNortnern@users.noreply.github.com>
2021-10-08 07:36:27 -0500

    Add quick responsiveness

--- src/components/BrowseWrapper.vue
+++ src/components/BrowseWrapper.vue
@@ -2,7 +2,15 @@
   <div>
     <p v-if="title" class="font-bold text-2xl mb-3 text-center">{{ title }}</p>
     <slot name="top" />
-    <div class="grid grid-cols-5 gap-3">
+    <div
+      class="
+        grid grid-cols-2
+        sm:grid-cols-3
+        md:grid-cols-4
+        lg:grid-cols-5
+        gap-3
+      "
+    >
       <slot />
     </div>
   </div>
